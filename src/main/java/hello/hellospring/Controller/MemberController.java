4e2598fc63591d3e13e05b4edddf4f1d2e6f8ceb
JAEHYUN <wogustkfkd11@gmail.com>
2022-11-27 20:50:58 +0900

    MemberController commit

--- src/main/java/hello/hellospring/Controller/MemberController.java
+++ src/main/java/hello/hellospring/Controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.Controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,4 +36,35 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new") //url창에 Enter (조회할 때 사용)
+    public String createForm(){
+        return "members/createMemberForm"; // resources/ templates에서 createMemberForm.html 찾음
+    }
+
+    @PostMapping("/members/new") // .html의 form에 데이터를 넘겨서 받을 때 @PostMapping 사용
+    public String create(MemberForm form){ // createMemberForm.html의 input에서 넘어온 name을 MemberForm의 name에 저장
+        Member member = new Member();
+        member.setName(form.getName()); // form.getName()된 name을 =MemberForm의 private String name;에 넣어줌
+
+        /*System.out.println("member= " +member.getName());*/
+
+        memberService.join(member);
+
+        return "redirect:/"; //다시 home화면으로
+    }
+
+    @GetMapping("/members")
+    /**
+     * Model 객체: Controller에서 생성한 데이터를 담아서 View로 전달할 때 사용하는 객체, map구조로 저장됨(key:value)
+     */
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); // members List 전체를 model에 담아서 화면 출력
+        return "members/memberList";
+    }
 }
+
+/**
+ * 최근 파일 보기: Ctrl + E
+ */
