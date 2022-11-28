f36a21e74855dffa2f4c386a72707eb5d7528c50
JAEHYUN <wogustkfkd11@gmail.com>
2022-11-28 09:01:57 +0900

    fix construction injection commit

--- src/main/java/hello/hellospring/service/springConfig.java
+++ src/main/java/hello/hellospring/service/springConfig.java
@@ -2,13 +2,12 @@ package hello.hellospring.service;
 
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
+
 
 /**
  * memberService()와  memberRepository()를 스프링 빈에 등록
@@ -26,12 +25,12 @@ public class springConfig {
 
 
     @Bean //@Configuration 보고 @Bean을 스프링 빈에 등록시킴
-    public MemberService memberService() throws SQLException {
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
     @Bean
-    public MemberRepository memberRepository() throws SQLException {
+    public MemberRepository memberRepository() {
         //return new MemoryMemberRepository(); //MemberRepository는 인터페이스라 new 안됨
         return new JdbcMemberRepository(dataSource);
     }
