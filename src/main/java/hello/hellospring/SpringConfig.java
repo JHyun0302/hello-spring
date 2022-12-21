f94d9e1a9631e7f8d41a2a6a50aee98eb7f10ced
jaehyun <wogustkfkd11@gmail.com>
2022-12-21 22:47:06 +0900

    memory, jdbc, jdbctemplate

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,36 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * memberService()와  memberRepository()를 스프링 빈에 등록
+ */
+@Configuration
+public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    @Bean //@Configuration 보고 @Bean을 스프링 빈에 등록시킴
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        //return new MemoryMemberRepository(); //MemberRepository는 인터페이스라 new 안됨
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+    }
+}
