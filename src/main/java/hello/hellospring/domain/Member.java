20d244dc8081925f9a0f3d27db4805dcf150baf7
jaehyun <wogustkfkd11@gmail.com>
2022-12-23 09:37:35 +0900

    @Entity

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,16 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //JPA가 관리하는 객체
 public class Member {
+    @Id //PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // db에 값 넣으면 자동으로 id 생성되는 것 = identity
     private Long id; // 시스템이 저장하는 id
+    //@Column(name = "username") //Column의 내용 = username
     private String name;
 
     public Long getId() { // Getter&Setter: Alt + Insert
